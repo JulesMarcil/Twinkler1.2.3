80364f9b2c2d7301c3ed8b99992414f836eaa2db
JulesMarcil <jules.marcilhacy@gmail.com>
2013-07-27 13:25:38 +0200

    test commit

---
+++ testTests.h
@@ -0,0 +1,13 @@
+//
+//  testTests.h
+//  testTests
+//
+//  Created by Jules Marcilhacy on 16/07/13.
+//  Copyright (c) 2013 Jules Marcilhacy. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface testTests : SenTestCase
+
+@end
