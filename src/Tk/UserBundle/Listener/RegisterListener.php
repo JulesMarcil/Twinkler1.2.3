7a2443b55831bc7fc9a5cd51328204cbefa2e9a8
JulesMarcil <jules.marcilhacy@gmail.com>
2013-09-15 18:41:59 +0100

    create group before login

--- src/Tk/UserBundle/Listener/RegisterListener.php
+++ src/Tk/UserBundle/Listener/RegisterListener.php
@@ -7,6 +7,13 @@ use FOS\UserBundle\Event\FormEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Doctrine\Bundle\DoctrineBundle\Registry as Doctrine;
 
+
+use Symfony\Component\Routing\Router; 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
 use Tk\UserBundle\Entity\Member,
 	Tk\GroupBundle\Entity\TGroup,
 	Tk\ExpenseBundle\Entity\Expense,
@@ -20,15 +27,26 @@ class RegisterListener implements EventSubscriberInterface
 	/** @var \Doctrine\ORM\EntityManager */
 	private $em;
 	
+    /** @var \Symfony\Component\Routing\Router */
+    private $router;
+    
+    /** @var \Symfony\Component\EventDispatcher\EventDispatcher */
+    private $dispatcher;
+
 	/**
 	 * Constructor
 	 * 
 	 * @param SecurityContext $securityContext
 	 * @param Doctrine        $doctrine
+     * @param Router          $router
+     * @param Dispatcher      $dispatcher
 	 */
-	public function __construct(Doctrine $doctrine)
+
+	public function __construct(Doctrine $doctrine, Router $router, EventDispatcher $dispatcher)
 	{
-		$this->em = $doctrine->getEntityManager();
+		$this->em         = $doctrine->getEntityManager();
+        $this->router     = $router;
+        $this->dispatcher = $dispatcher;
 	}
 
 	public static function getSubscribedEvents()
@@ -45,8 +63,32 @@ class RegisterListener implements EventSubscriberInterface
 	 */
 	public function onRegistrationSuccess(FormEvent $event)
 	{
-        /*
         $user = $event->getForm()->getData();
+        $picture = $this->em ->getRepository('TkUserBundle:ProfilePicture')->find(1);           
+        $user->setPicture($picture);
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $session = $event->getRequest()->getSession();
+        $group_id = $session->get('created_group_id');
+        if ($group_id) {
+            $group = $this->em->getRepository('TkGroupBundle:TGroup')->find($group_id);
+
+            $member = new Member();
+            $member->setUser($user);
+            $member->setName($user->getUsername());
+            $member->setTGroup($group);
+            $user->setCurrentMember($member);
+            $this->em->persist($member);
+            $this->em->persist($user);
+            $this->em->flush();
+
+            $session->remove('created_group_id');
+
+            $this->dispatcher->addListener(KernelEvents::RESPONSE, array($this, 'onKernelResponse'));
+        }
+
+        /*
 	    
 	    $currency = $this->em->getRepository('TkGroupBundle:Currency')->find(1);	
 
@@ -99,9 +141,6 @@ class RegisterListener implements EventSubscriberInterface
 		$message2->setAuthor($arnaud_member);
 		$message2->setGroup($group);
 
-        $picture = $this->em ->getRepository('TkUserBundle:ProfilePicture')->find(1);	        
-        $user->setPicture($picture);
-
         $this->em->persist($user);
         $this->em->persist($group);
         $this->em->persist($member);
@@ -113,4 +152,10 @@ class RegisterListener implements EventSubscriberInterface
         $this->em->flush();
         */
 	}
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        $response = new RedirectResponse($this->router->generate('tk_group_add_members'));
+        $event->setResponse($response);
+    }
 }
--- src/Tk/WelcomeBundle/Controller/DefaultController.php
+++ src/Tk/WelcomeBundle/Controller/DefaultController.php
@@ -5,6 +5,10 @@ namespace Tk\WelcomeBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Tk\WelcomeBundle\Entity\Subscribe;
 
+use Tk\GroupBundle\Entity\TGroup;
+use Tk\GroupBundle\Form\TGroupType;
+use Tk\ListBundle\Entity\Lists;
+
 class DefaultController extends Controller
 {
     public function subscribeAction(){
@@ -13,8 +17,8 @@ class DefaultController extends Controller
         $subscribe->setDate(new \DateTime('now'));
 
         $form = $this->createFormBuilder($subscribe)
-            ->add('email', 'email')
-            ->getForm();
+                     ->add('email', 'email')
+                     ->getForm();
 
         $request = $this->get('request');
 
@@ -56,5 +60,45 @@ class DefaultController extends Controller
 
     public function contactAction(){
         return $this->render('TkWelcomeBundle:Links:contact.html.twig');
+    }
+
+    public function newGroupAction()
+    {
+        $group = new TGroup();
+
+        $form = $this->createForm(new TGroupType(), $group);
+
+        $request = $this->get('request');
+
+        if ($request->isMethod('POST')) {
+
+            $form->bind($request);
+
+            if ($form->isValid()) {
+
+                $em = $this->getDoctrine()->getEntityManager();
+
+                $group->setInvitationToken($group->generateInvitationToken());
+                $todolist = new Lists();
+                $todolist->setName('Todo List');
+                $todolist->setGroup($group);
+                $shoppinglist = new Lists();
+                $shoppinglist->setName('Shopping List');
+                $shoppinglist->setGroup($group);
+                $em->persist($todolist);
+                $em->persist($shoppinglist);
+                $em->persist($group);
+                $em->flush();
+
+                $session = $this->get('session');
+                $session->set('created_group_id', $group->getId());
+
+                return $this->redirect($this->generateUrl('fos_user_registration_register'));
+            }
+        }
+
+        return $this->render('TkWelcomeBundle:CreateGroup:groupName.html.twig', array(
+            'form' => $form->createView(),
+            ));        
     }
 }
