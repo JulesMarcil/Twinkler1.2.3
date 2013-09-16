aad1e907526287e0aa878b9fa0d44bd72101c3a4
ArnaudDri <arnaud.drizard@gmail.com>
2013-09-16 22:17:16 +0100

    scroll

--- web/js/group.js
+++ web/js/group.js
@@ -10,6 +10,32 @@
 		$("#balance-slimscroll").niceScroll();
 	}
 
+
+	$.scrollTo = $.fn.scrollTo = function(x, y, options){
+	    if (!(this instanceof $)) return $.fn.scrollTo.apply($('html, body'), arguments);
+
+	    options = $.extend({}, {
+	        gap: {
+	            x: 0,
+	            y: 0
+	        },
+	        animation: {
+	            easing: 'swing',
+	            duration: 600,
+	            complete: $.noop,
+	            step: $.noop
+	        }
+	    }, options);
+
+	    return this.each(function(){
+	        var elem = $(this);
+	        elem.stop().animate({
+	            scrollLeft: !isNaN(Number(x)) ? x : $(y).offset().left + options.gap.x,
+	            scrollTop: !isNaN(Number(y)) ? y : $(y).offset().top + options.gap.y
+	        }, options.animation);
+	    });
+	};
+
 	/*--------CHARTS--------*/
 
 	var graphColor=function(graphData){
@@ -241,6 +267,8 @@
 					input.val('');
 					input.focus();
 					// rappeler les fonctions de mise en forme
+
+					$("#message-list").animate({ scrollTop: 100000 }, "fast");
 				});
 		    }
 		});
