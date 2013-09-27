269eab7d45800c7ef463ef21270d8653bcff650d
ArnaudDri <arnaud.drizard@gmail.com>
2013-09-27 14:32:21 +0100

    bug fixed on navbar (scroll)

--- web/js/group.js
+++ web/js/group.js
@@ -318,7 +318,7 @@
 				
 				var $cache = $('#navbar-row'); 
 				var $cacheItem = $('#navbar-expenses');
-				if ($(window).scrollTop() > oritop) {
+				if ($(window).scrollTop() > oritop && ($(document).height()-32>$(window).height())) {
 					$("#navbar-"+activePage).css({'border-top-left-radius':'0px','border-top-right-radius':'0px'});
 					$cache.addClass("navbar-scroll");
 					$cache.css({'position': 'fixed', 'top': '40px','z-index':'9999999999'}); 
