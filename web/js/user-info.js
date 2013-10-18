f82e1f61971454992b089b2cf31c30984f5d08f2
JulesMarcil <jules.marcilhacy@gmail.com>
2013-10-18 17:43:28 +0100

    spinner on group choice

--- web/js/user-info.js
+++ web/js/user-info.js
@@ -5,6 +5,14 @@ $(document).ready(function() {
 	}, function() {
 		$('.navbar .dropdown').find('.dropdown-menu').first().stop(true, true).delay(0).slideUp()
 	});
+
+	// spinner when group clicked
+	$('.group-link').on('click', function(e){
+		e.preventDefault();
+		var id = $(this).data('id');
+		$('.group-spinner').fadeOut();
+		$('#spinner-'+id).fadeIn();
+	})
 });
 
 $('#group-dropdown-span').hover(function() {
